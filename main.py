0acfbbc28364fff08e7024cdfb6d229206136294
EboYu <yyb784@gmail.com>
2020-03-04 21:54:10 +0800

    UML format

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+#!/usr/bin/python3.5
 import argparse
+import xml.etree.ElementTree as ET
+import plantuml
+
+def parseUML(umlFile):
+    # uml can be a sequence diagram or a state diagram
+
+    tree = ET.parse(umlFile)
 
 
 def main(inputFile, outputFile):
+    # generate a figure of uml iva plantuml
+    #pl = plantuml.PlantUML()
+    #pl.processes(inputFile)
     i = 0
 
 
